22320b58c56f4c1d16ef96965ff5ff2ea0422b30
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-09 09:09:47 +0100

    Added DaisyUI theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -6,35 +6,21 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        blue: {
-          "1": "#EEF6FC",
-          "5": "#DCEDF9",
-          "10": "#CBE5F6",
-          "20": "#BADCF3",
-          "30": "#A8D3F0",
-          "40": "#97CAED",
-          "50": "#86C2EA",
-          "100": "#74B9E7",
-          "200": "#63BOE3",
-          "300": "#52A7E0",
-          "400": "#3498DB",
-          "500": "#2F96DA",
-          "600": "#258CD0",
-          "700": "#2280BF",
-          "800": "#1F74AD",
-          "900": "#1C699C",
-        },
+  plugins: [require("daisyui")],
+  themes: [
+    {
+      mytheme: {
+        primary: "#9333ea",
+        secondary: "#A67EF1",
+        accent: "#C0A3F5",
+        neutral: "#8b5cf6",
+        "base-100": "#f3f4f6",
+        info: "#62CCF9",
+        success: "#2CC98F",
+        warning: "#FBC337",
+        error: "#F76464",
       },
     },
-  },
-  plugins: [],
+  ],
 };
-export default config
+export default config;
