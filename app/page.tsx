c8beefd960c15d38146bd7b8e598fa5dd76203aa
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-12 18:08:59 +0100

    Added static assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3 className="font-bold text-5xl capitalize">hello world</h3>
-      <h3 className="font-bold text-5xl capitalize">good evening world</h3>
-      <p>my name is adedoyin emmanuel</p>
-    </main>
+    <section>
+      <header>
+
+      </header>
+
+
+      <section></section>
+    </section>
   );
 }
