f18c4169fc050d13a041ac6adf32fa330dc28eb0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-15 12:22:50 +0100

    Updated sidenav

---
+++ app/components/MobileNav/index.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { BiMessageRoundedDots } from "react-icons/bi";
+import { BsCameraVideo } from "react-icons/bs";
+import { IoAnalytics, IoSettingsOutline } from "react-icons/io5";
+import Text from "../Text";
+interface MobileNavProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const MobileNav = ({ className, children, ...others }: MobileNavProps) => {
+  const currentPath = usePathname();
+  return (
+    <section
+      className={`w-screen flex items-center justify-between fixed bottom-0 left-0 md:hidden  bg-gray-100 h-20 z-10 p-2 ${className}`}
+      {...others}
+    >
+      {children}
+
+      <Link
+        href={"/user/dashboard"}
+        className={`${
+          currentPath.includes("dashboard") && "text-secondary"
+        } transition-colors hover:text-secondary duration-100 ease-in flex flex-col items-center justify-center gap-y-2`}
+      >
+        <IoAnalytics className="h-6 w-6" />
+        <Text className="text-[13px]">Dashboard</Text>
+      </Link>
+
+      <Link
+        href={"/user/appointments"}
+        className={`${
+          currentPath.includes("appointments") && "text-secondary"
+        } transition-colors hover:text-secondary duration-100 ease-in flex flex-col items-center justify-center gap-y-2`}
+      >
+        <BsCameraVideo className="h-6 w-6" />
+        <Text className="text-[13px]">Appointments</Text>
+      </Link>
+
+      <Link
+        href={"/user/messages"}
+        className={`${
+          currentPath.includes("messages") && "text-secondary"
+        } transition-colors hover:text-secondary duration-100 ease-in flex flex-col items-center justify-center gap-y-2`}
+      >
+        <BiMessageRoundedDots className="h-6 w-6" />
+        <Text className="text-[13px]">Messages</Text>
+      </Link>
+
+      <Link
+        href={"/user/settings"}
+        className={`${
+          currentPath.includes("settings") && "text-secondary"
+        } transition-colors hover:text-secondary duration-100 ease-in flex flex-col items-center justify-center gap-y-2`}
+      >
+        <IoSettingsOutline className="h-6 w-6" />
+        <Text className="text-[13px]">Settings</Text>
+      </Link>
+    </section>
+  );
+};
+
+export default MobileNav;
--- app/components/SidebarLayout/index.tsx
+++ app/components/SidebarLayout/index.tsx
@@ -2,13 +2,94 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 import { BiMessageRoundedDots } from "react-icons/bi";
 import { BsCameraVideo } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
 import { IoAnalytics, IoSettingsOutline } from "react-icons/io5";
+import MobileNav from "../MobileNav";
 import Text from "../Text";
 
-const SidebarLayout = () => {
+interface SidebarLayoutProps{
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const SidebarLayout = ({className, children}: SidebarLayoutProps) => {
+  const currentPath = usePathname();
+
+  return (
+    <section className="parent h-screen w-screen flex justify-between">
+      <div className={`bg-zinc-100 h-screen hidden md:flex md:w-3/12  lg:w-2/12 items-center justify-start flex-col  p-0  fixed ${className} `}>
+        <section className="sidebar-header my-4">
+          <h2 className="font-bold text-[20px] capitalize ">caresync</h2>
+        </section>
+
+        <section className="w-11/12 p-2">
+          <Link
+            href={"/user/dashboard"}
+            className={`dashboard cursor-pointer ${
+              currentPath.includes("dashboard") && "bg-accent text-white"
+            } hover:bg-accent duration-100 ease-in hover:text-white transition-colors flex items-center gap-x-4 bg-gray-200 p-5 rounded my-4`}
+          >
+            <IoAnalytics className="w-6 h-6" />
+            <Text>Dashboard</Text>
+          </Link>
+
+          <Link
+            href="/user/appointments"
+            className={`dashboard cursor-pointer ${
+              currentPath.includes("appointments") && "bg-accent text-white"
+            } hover:bg-accent duration-100 ease-in hover:text-white transition-colors flex items-center gap-x-4 bg-gray-200 p-5 rounded my-4`}
+          >
+            <BsCameraVideo className="w-6 h-6" />
+            <Text>Appointments</Text>
+          </Link>
+
+          <Link
+            href="/user/messages"
+            className={`dashboard cursor-pointer ${
+              currentPath.includes("messages") && "bg-accent text-white"
+            } hover:bg-accent duration-100 ease-in hover:text-white transition-colors flex items-center gap-x-4 bg-gray-200 p-5 rounded my-4`}
+          >
+            <BiMessageRoundedDots className="w-6 h-6" />
+            <Text>Messages</Text>
+          </Link>
+
+          <Link
+            href="/user/settings"
+            className={`dashboard cursor-pointer ${
+              currentPath.includes("settings") && "bg-accent text-white"
+            } hover:bg-accent duration-100 ease-in hover:text-white transition-colors flex items-center gap-x-4 bg-gray-200 p-5 rounded my-4`}
+          >
+            <IoSettingsOutline className="w-6 h-6" />
+            <Text>Settings</Text>
+          </Link>
+
+          <Link
+            href="logout"
+            className={`dashboard cursor-pointer ${
+              currentPath.includes("logout") && "bg-accent text-white"
+            } hover:bg-accent duration-100 ease-in hover:text-white transition-colors flex items-center gap-x-4 bg-gray-200 p-5 rounded my-4`}
+          >
+            <FiLogOut className="w-6 h-6" />
+            <Text>Logout</Text>
+          </Link>
+        </section>
+      </div>
+  
+      <section className="w-full md:w-3/4 lg:w-5/6 md:p-8 p-2 pl-1/4 overflow-y-auto ml-auto">
+        {children}
+    
+      </section>
+      ;
+      <MobileNav />
+    </section>
+  );
+};
+
+
+const HospitalSidebarLayout = () => {
   const currentPath = usePathname();
 
   return (
