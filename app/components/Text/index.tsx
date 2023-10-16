09a3287406c447dc9d261d0881aba948a41651d3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-16 18:45:47 +0100

    Updated the settings page

--- app/components/Text/index.tsx
+++ app/components/Text/index.tsx
@@ -3,10 +3,19 @@ import React from "react";
 interface TextProps {
   className?: string;
   children: React.ReactNode;
+  noCapitalize?: boolean;
 }
 
-const Text = ({ className, children }: TextProps) => {
-  return <p className={`text-md capitalize ${className}`}>{children}</p>;
+const Text = ({ className, children, noCapitalize }: TextProps) => {
+  return (
+    <p
+      className={`text-md ${
+        noCapitalize ? "lowercase" : "capitalize"
+      } ${className}`}
+    >
+      {children}
+    </p>
+  );
 };
 
 export default Text;
