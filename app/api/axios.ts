0cfe6c98863debe526b8fc7d9f1b52bae2667778
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-17 10:40:56 +0100

    Finished the base axios request file

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const apiBaseUrl = process.env.NEXT_BASE_URL || "http://localhost:2800/api";
 
 const Axios = axios.create({
-    
-})
+    baseURL: apiBaseUrl
+});
+
+export default Axios;
