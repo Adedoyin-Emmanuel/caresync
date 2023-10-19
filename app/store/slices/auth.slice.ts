f9b14f949fd93f5423b4a6b1e6d23c3d6a79efb1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-19 01:48:04 +0100

    Feat: Finished the authSlice

---
+++ app/store/slices/auth.slice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    isAuthenticated: false,
+    role: "",
+    data: null
+}
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        loginUser: (state, action) => {
+            state.isAuthenticated = true;
+            state.role = "user";
+            state.data = action.payload.data
+        },
+
+        loginHospital: (state, action) => {
+            state.isAuthenticated = true;
+            state.role = "hospital";
+            state.data = action.payload.data
+        },
+
+        logout: (state, action) => {
+            state.isAuthenticated = false;
+            state.role = "";
+            state.data = null
+        }
+    }
+});
+
+
+export const { loginUser, loginHospital, logout } = authSlice.actions;
+export default authSlice.reducer;
