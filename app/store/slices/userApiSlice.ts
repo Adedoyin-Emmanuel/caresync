c074026c45208b809fa534aa6f2129475c30e8bd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-19 02:09:52 +0100

    Feat: added the userSlice

---
+++ app/store/slices/userApiSlice.ts
@@ -0,0 +1,65 @@
+import { apiSlice } from "./api.slice";
+const USERS_URL = "/user";
+const HOSPITALS_URL = "/hospital";
+const AUTH_URL = "/auth";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    logout: builder.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+
+    verifyEmail: builder.query({
+      query: (data) => ({
+        url: `${AUTH_URL}/verify-email`,
+        method: "GET",
+        body: data,
+      }),
+    }),
+
+    confirmEmail: builder.query({
+      query: (data) => ({
+        url: `${AUTH_URL}/confirm-email`,
+        method: "GET",
+        body: data,
+      }),
+    }),
+
+    forgotPassword: builder.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/forgot-password`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    registerUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    registerHospital: builder.mutation({
+      query: (data) => ({
+        url: `${HOSPITALS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    
+  }),
+});
