8e49b8f86b2b5e36131d9c829a1224bf876b8049
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-03 03:17:44 +0100

    Updated app UI

--- app/components/DashboardCard/DashboardCard.tsx
+++ app/components/DashboardCard/DashboardCard.tsx
@@ -1,19 +1,56 @@
+"use client";
+import { useEffect, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
-"use client"
-import React from "react";
-
-interface IndexProps {
-    
+interface DashboardCardProps {
+  className?: string;
+  appointments: number | string;
 }
 
-const Index = ()=> {
+const DashboardCard = ({
+  className,
+  appointments,
+  ...others
+}: DashboardCardProps) => {
+  const [currentIcon, setCurrentIcon] = useState(AiOutlineEye);
+  const [toggler, setToggler] = useState(true);
+  const [totalAppointment, setTotalAppointment] = useState(appointments);
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-        </div>
-    );  
-}
+  const handleIconClick = () => {
+    setToggler(!toggler);
+  };
 
-export default Index;
-    
+  useEffect(() => {
+    setCurrentIcon(toggler ? AiOutlineEye : AiOutlineEyeInvisible);
+    setTotalAppointment(toggler ? appointments : "****");
+  }, [toggler]);
+  return (
+    <section
+          className={`rounded-lg md:hidden bg-purple-200 gap-y-2 p-4 w-full ${className}`}
+      {...others}
+    >
+      <section className="header flex items-center justify-between">
+        <section className="first flex items-center gap-x-1">
+          <p className="capitalize text-[12px]">total appoinments</p>
+          <section className="icon-container" onClick={handleIconClick}>
+            {currentIcon}
+          </section>
+        </section>
+        <section className="end flex items-center gap-x-1">
+          <p className="capitalize text-[12px]">healthcare history</p>
+
+          <MdOutlineKeyboardArrowRight />
+        </section>
+      </section>
+      <h3 className="text-[18px] font-bold my-2">{totalAppointment}</h3>
+      <section className="w-full flex items-end justify-end">
+        <section className="new-appointment bg-accent rounded-[30px] capitalize w-28 text-[12px] text-center p-1  text-white">
+          appointment
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default DashboardCard;
