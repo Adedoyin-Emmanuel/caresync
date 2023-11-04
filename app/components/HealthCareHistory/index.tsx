03caedce4bf96b1bac5599639ff3480261afd9d3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-04 18:38:32 +0100

    Fix: Fixed the health care history bug

--- app/components/HealthCareHistory/index.tsx
+++ app/components/HealthCareHistory/index.tsx
@@ -1,7 +1,7 @@
 import {
   healthCareHistoryProps,
-  useGetHospitalByIdMutation,
-  useGetUserByIdMutation,
+  useGetHospitalByIdQuery,
+  useGetUserByIdQuery,
 } from "@/app/store/slices/user.slice";
 import moment from "moment";
 import Link from "next/link";
@@ -22,19 +22,21 @@ const HealthCareHistoryCard = ({
   href,
   userType,
 }: HealthCareHistoryCardProps) => {
-  const [getHospitalById] = useGetHospitalByIdMutation();
-  const [getUserById] = useGetUserByIdMutation();
-
-  const [attenderName, setAttenderName] = useState();
+  const [attenderName, setAttenderName] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     const abortController = new AbortController();
     const { signal } = abortController;
+
     const fetchData = async () => {
       try {
-        const getUser = userType == "user" ? getUserById : getHospitalById;
-        const response: any = await getUser(attender);
-        setAttenderName(response.data.data.username);
+        if (userType === "user") {
+          const { data } = await useGetUserByIdQuery(attender);
+          setAttenderName(data.data.username);
+        } else if (userType === "hospital") {
+          const { data } = await useGetHospitalByIdQuery(attender);
+          setAttenderName(data.data.username)
+        }
       } catch (error: any) {
         if (error.name === "AbortError") {
           console.log("Request was aborted due to unmounting.");
@@ -49,20 +51,19 @@ const HealthCareHistoryCard = ({
     return () => {
       abortController.abort();
     };
-  }, []);
+  }, [attender, userType]);
 
   return (
     <Link href={href}>
-          <section className={`appointment bg-gray-100 transition-colors ease-in hover:bg-purple-100 flex items-center justify-between p-4 rounded cursor-pointer my-4 ${className}`}>
+      <section className={`appointment bg-gray-100 transition-colors ease-in hover:bg-purple-100 flex items-center justify-between p-4 rounded cursor-pointer my-4 ${className}`}>
         <section className="icon bg-accent text-white p-3 flex items-center justify-center rounded">
           <HiOutlineShieldCheck className="w-6 h-6" />
         </section>
-
         <section className="other-content w-11/12 flex items-center justify-around">
           <Text className="text-sm font-bold" noCapitalize={true}>
             @{attenderName}
           </Text>
-          <div className="text-[13px] lowercase flex items-center  gap-x-1">
+          <div className="text-[13px] lowercase flex items-center gap-x-1">
             <LuTimer className="w-5 h-5" />
             {moment(new Date(createdAt)).startOf("seconds").fromNow()}
           </div>
@@ -71,7 +72,5 @@ const HealthCareHistoryCard = ({
     </Link>
   );
 };
-
-
 
 export { HealthCareHistoryCard };
