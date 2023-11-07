604bb7843f244db68d919add2f1ae35ddc7649d5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-07 13:51:02 +0100

    Fix: Adjusted the chat input element

--- app/user/messages/[messageId]/page.tsx
+++ app/user/messages/[messageId]/page.tsx
@@ -36,20 +36,58 @@ const Messages = () => {
                   </div>
                 </div>
 
-            
-
                 <div className="mb-4 receiver">
                   <div className="max-w-[70%] bg-slate-100 p-4 rounded-md text-sm">
-                 I dey my bro. How far with the project? Your babe nko?
+                    I dey my bro. How far with the project? Your babe nko?
                   </div>
                 </div>
-                <div className="my-10 md:p-2 w-full ">
+                {/* <div className="my-10 md:p-2 w-full ">
                   <input
                     type="text"
                     placeholder="Type a message..."
                     className="w-full border border-gray-300 p-4 rounded-[30px]"
                   />
-                </div>
+
+                  <button type="submit" className="absolute top-9 right-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="text-sky-500"
+                    >
+                      <path
+                        d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </button>
+                </div> */}
+
+                <form className="my-10 md:p-2 w-full flex">
+                  <input
+                    type="text"
+                    placeholder="Type a message..."
+                    className="w-full outline-none border-2 border-purple-300 focus:border-accent hover:border-accent transition-all duration-150 ease-in my-5 p-4 rounded-[30px] block"
+                  />
+                  <button
+                    type="submit"
+                    className="transform -translate-x-11  5"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="text-accent block"
+                    >
+                      <path
+                        d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </button>
+                </form>
               </div>
             </section>
           </section>
