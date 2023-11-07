3fcbfe6a5a7f335447ae26443089468922bfa555
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-07 05:54:44 +0100

    Added chat UI

--- app/user/messages/[messageId]/page.tsx
+++ app/user/messages/[messageId]/page.tsx
@@ -1,19 +1,54 @@
+import Button from "@/app/components/Button";
+import SidebarLayout from "@/app/components/SidebarLayout";
+import Text from "@/app/components/Text";
 
-"use client"
+const Messages = () => {
+  return (
+    <div className="w-screen h-screen bg-zinc-50">
+      <SidebarLayout>
+        <section className="appointments my-5">
+          <section className="messages-section my-5">
+            <section className="user-details flex items-center w-full gap-x-5 p-1">
+              <div className="avatar online">
+                <div className="w-12 rounded-full">
+                  <img src="https://api.dicebear.com/7.x/micah/svg?seed=bloom" />
+                </div>
+              </div>
 
-import React from "react";
+              <Text className="font-semibold">Fasakin henry</Text>
+            </section>
+            <section className="network-tab w-full items-center justify-center my-5">
+              <Text className="text-red-500 text-sm text-center">
+                no internet connection
+              </Text>
+            </section>
+            <section className="h-screen w-full flex flex-col">
+              <div className="flex-grow p-3  overflow-y-auto">
+                <div className="mb-4 receiver">
+                  <div className="max-w-[70%] bg-slate-100 p-4 rounded-md text-sm">
+                    Hi, emmanuel. How are you doing today?
+                  </div>
+                </div>
 
-import { useRouter } from "next/navigation";
+                <div className="mb-4 sender">
+                  <div className="max-w-[70%] bg-purple-200  p-4 rounded-md ml-auto text-sm">
+                    What's up chief? Am good.
+                  </div>
+                </div>
+              </div>
+              <div className="p-4">
+                <input
+                  type="text"
+                  placeholder="Type a message..."
+                  className="w-full border border-gray-300 p-2 rounded-md"
+                />
+              </div>
+            </section>
+          </section>
+        </section>
+      </SidebarLayout>
+    </div>
+  );
+};
 
-const page = ({ params }: { params: { messageId: string } }) => {
-
-    return (
-        <div>
-            <h1>page works!</h1>
-            <p>{params.messageId}</p>
-        </div>
-    );  
-}
-
-export default page;
-    
+export default Messages;
