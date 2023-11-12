db3f9419eecd99863c464a0c1016c2921e9d1f7b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-12 19:35:04 +0100

    Fix

--- middleware.ts
+++ middleware.ts
@@ -13,24 +13,31 @@ export function middleware(request: NextRequest) {
     path === "/auth/verified" ||
     path === "/auth/reset-password";
 
-  const token = request.cookies.get("refreshToken")?.value || null;
-  const tokenData: any = token ? jwt.decode(token) : null;
+  const token = request.cookies.get("refreshToken")?.value;
+  const tokenData: any = token && jwt.decode(token);
 
   // if it is a protected route and there is no token
-  if (!isPublicPath && !tokenData) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-
-  if (isPublicPath && tokenData) {
-    if (tokenData?.role === "user") {
-      return NextResponse.redirect(new URL("/user/dashboard", request.url));
-    } else if (tokenData?.role === "hospital") {
-      return NextResponse.redirect(new URL("/hospital/dashboard", request.url));
-    } else {
+  if (!tokenData) {
+    if (!isPublicPath) {
+      console.log("Trying to access protected route without token");
       return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   }
 
+  if (tokenData) {
+    if (isPublicPath) {
+      if (tokenData?.role === "user") {
+        return NextResponse.redirect(new URL("/user/dashboard", request.url));
+      } else if (tokenData?.role === "hospital") {
+        return NextResponse.redirect(
+          new URL("/hospital/dashboard", request.url)
+        );
+      } else {
+        console.log("No role found");
+        //return NextResponse.redirect(new URL("/auth/login", request.url));
+      }
+    }
+  }
   if (tokenData) {
     // Check if the path starts with "/user/"
     if (path.startsWith("/user/")) {
