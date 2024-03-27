6d2c0dee3bbf793529fb945b95b40d99d212c930
= <=>
2024-03-27 23:05:25 +0100

    Feat: Implemented the Seo component

--- app/components/Seo/Seo.tsx
+++ app/components/Seo/Seo.tsx
@@ -1,19 +1,61 @@
-
-"use client"
 import React from "react";
+import Head from "next/head";
+import siteConfig from "@/config/site";
 
-interface IndexProps {
-    
+interface SeoProps {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  keywords?: string;
 }
 
-const Index = ()=> {
+const Seo = ({ title, description, image, url, keywords }: SeoProps) => {
+  return (
+    <>
+      <title>{title ?? siteConfig.headline}</title>
+      <meta
+        name="description"
+        content={description ?? siteConfig.description}
+      />
+      <meta name="keywords" content={keywords ?? siteConfig.keywords} />
+      <meta name="image" content={image ?? siteConfig.image} />
+      <meta name="url" content={url ?? siteConfig.domain} />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-        </div>
-    );  
-}
+      <meta property="og:title" content={title ?? siteConfig.headline} />
+      <meta
+        property="og:description"
+        content={description ?? siteConfig.description}
+      />
+      <meta property="og:image" content={image ?? siteConfig.image} />
+      <meta property="og:url" content={url ?? siteConfig.domain} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Caresync" />
 
-export default Index;
-    
+      <meta property="twitter:title" content={title ?? siteConfig.headline} />
+      <meta
+        property="twitter:description"
+        content={description ?? siteConfig.description}
+      />
+      <meta name="twitter:image" content={image ?? siteConfig.image} />
+      <meta property="twitter:url" content={url ?? siteConfig.domain} />
+      <meta property="twitter:type" content="website" />
+      <meta name="twitter:site" content="@emmysoft_tm" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@emmysoft_tm" />
+
+      <link rel="icon" href="/favicon.ico" />
+
+      <meta name="apple-mobile-web-app-title" content="Caresync" />
+
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="application-name" content="Caresync" />
+      <meta name="msapplication-TileImage" content="/logo.png" />
+    </>
+  );
+};
+
+export default Seo;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import { ReduxProvider } from "./store/provider";
 import { Toaster } from "react-hot-toast";
-
-export const metadata: Metadata = {
-  title: "Caresync",
-  description: "Bridging health care with technology",
-};
+import Seo from "./components/Seo/Seo";
 
 export default function RootLayout({
   children,
@@ -16,6 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="mytheme">
+      <Seo />
       <body className="bg-zinc-50">
         <Toaster />
         <ReduxProvider>{children}</ReduxProvider>
